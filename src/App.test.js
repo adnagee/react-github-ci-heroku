c1c7923bf6b7bac24b7ab2993d7005cab61293de
Alicia Dennis-Nagee <526473+gb96@users.noreply.github.com>
2022-03-12 08:47:59 -0400

    updates test file

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/connect a React app to both github and heroku/i);
   expect(linkElement).toBeInTheDocument();
 });
